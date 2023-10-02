0b35f1569b4e0a359087d794220f6385cba51b9d
MaruselPlay <maruselplay@gmail.com>
2023-10-02 18:40:09 +0100

    helloword

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWord{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println("Stone Roses: Ian, John");
+    }
+}
