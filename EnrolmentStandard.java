ba5365b81854ce98bbefb642f50d46a637f56315
Marusel <62075300+MaruselPlay@users.noreply.github.com>
2023-10-06 15:55:53 +0100

    enrolment 1

---
+++ EnrolmentStandard.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class EnrolmentStandard{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first digit of code: ");
+        String name = scanner.nextLine();
+        do{
+            
+        }while(name.isEmpty());
+    }
+}
