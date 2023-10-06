4f483d6d4f013ad84284483d63eb861e1105c7f5
Marusel <62075300+MaruselPlay@users.noreply.github.com>
2023-10-06 15:35:10 +0100

    SwimmingPool

---
+++ SwimmingPool.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SwimmingPool{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of the pool in metres: ");
+        String lengthString = scanner.nextLine();
+
+        System.out.print("Enter the width of the pool in metres: ");
+        String widthString = scanner.nextLine();
+
+        System.out.print("Enter the depth of the pool in metres: ");
+        String depthString = scanner.nextLine();
+
+        scanner.close();
+
+        try{
+            double length = Double.parseDouble(lengthString);
+            double width = Double.parseDouble(widthString);
+            double depth = Double.parseDouble(depthString);
+
+            System.out.println("The perimeter is " + (length * 2) + (width * 2));
+            System.out.println("The volume is " + length * width * depth);
+        }catch(NumberFormatException exeption){
+            System.out.println("Some of data entered, wasn't a valid number");
+        }
+    }
+}
