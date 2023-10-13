64f4bfd0b1e1160c4d4f5bf5f2a18351c954c561
Marusel <62075300+MaruselPlay@users.noreply.github.com>
2023-10-13 11:38:21 +0100

    Update Conversation.java

--- Conversation.java
+++ Conversation.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+/*This code letting the user to enter their name
+*Then this code outputs "hello, " and the name you've entered
+*Then this code creates a nickname for the user, based on thier name
+*/
+
 public class Conversation {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
