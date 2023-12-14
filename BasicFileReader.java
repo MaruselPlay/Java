e90d256bd312789544ecf9283603fe9612ca903f
Marusel <62075300+MaruselPlay@users.noreply.github.com>
2023-12-14 12:20:02 +0000

    update

--- BasicFileReader.java
+++ BasicFileReader.java
@@ -1,15 +1,26 @@
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 
 class BasicReader{
     public static void main(String[] args){
-        BufferedReader filereader = new BufferedReader(FileReader("file.txt"));
         while(true){
-            String line filereader.readLine();
-            if(line.equals("EOF")){
-                break;
+            try{
+                BufferedReader filereader = new BufferedReader(new FileReader("file.txt"));
+                while(true){
+                    try{
+                        String line = filereader.readLine();
+                        if(line.equals("EOF")){
+                            break;
+                        }
+                        System.out.println(line);
+                    }catch(Exception e){
+                        System.out.println("Failed to read the line");
+                    }
+                }
+            }catch(FileNotFoundException e){
+                System.out.println("File not found");
             }
-            System.out.println(line);
         }
     }
 }
