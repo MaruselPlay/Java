9a43f942e9526dc0058a41a3783ba7ce17371086
MaruselPlay <maruselplay@gmail.com>
2023-12-15 06:31:07 +0000

    filereaders update

--- AdvancedFileReader.java
+++ AdvancedFileReader.java
@@ -6,37 +6,51 @@ import java.util.Scanner;
 class AdvancedFileReader{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        BufferedReader fileReader;
-        String fileName = "";
         while(true){
             System.out.print("Enter the name of file: ");
             try{
-                fileName = scanner.nextLine();
-                fileReader = new BufferedReader(new FileReader(fileName)); //opens the file user has entered
+                String fileName = scanner.nextLine();
+                BufferedReader fileReader = new BufferedReader(new FileReader(fileName)); //opens the file user has entered
+
+                //The following block of code counts the number of lines in the file, to create a fixed length array later
+                int linesCount = 0;
+                try{
+                    BufferedReader linesCounter = new BufferedReader(new FileReader(fileName));
+                    while(linesCounter.readLine() != null) linesCount++;
+                    linesCounter.close();
+                }catch(Exception e){
+                    System.out.println("An error occured while reading the file");
+                }
+
+                String[] linesToStore = new String[linesCount];
+                linesCount = 0;
+                String line = "";
+                while(true){ //while loop to read lines from the file until end
+                    try{
+                        line = fileReader.readLine(); //reading next line from the file
+                    }catch(Exception e){
+                        System.out.println("Failed to read the line");
+                        break; // breaking the while loop
+                    }
+                    if(line.equals("EOF")){
+                        break; //breaks the while loop when line in file equals EOF
+                    }
+                    linesToStore[linesCount] = line; //stores current line in array
+                    linesCount++;
+                    System.out.println(line); //outputs current line
+                    System.out.println(linesCount); //outputs current count of lines
+                }
+
+                try{
+                    fileReader.close(); // closing the file
+                }catch(Exception e){ // catching any errors that potentionaly might come up
+                    System.out.println("An error occured while closing file");
+                }
                 break;
-            }catch(FileNotFoundException e){
+            }catch(FileNotFoundException e){ //file entered by the user, not found
                 System.out.println("File not found");
             }
         }
-        int count = 0;
-        String line = "";
-        int lines = 0;
-        linesCounter = new BufferedReader(new FileReader(fileName));
-        while (linesCounter.readLine() != null) lines++;
-        linesCounter.close();
-        String[] = new String[lines];
-
-        while(true){ //while loop to read lines from the file until end
-            try{
-                line = fileReader.readLine();
-            }catch(Exception e){}
-            if(line.equals("EOF")){
-                break; //breaks the while loop when line in file equals EOF
-            }
-            count++;
-            lines[count] = line; //stores current line in array
-            System.out.println(line); //outputs current line
-            System.out.println(line); //outputs current count of lines
-        }
+        scanner.close();
     }
 }
--- BasicFileReader.java
+++ BasicFileReader.java
@@ -4,23 +4,27 @@ import java.io.FileNotFoundException;
 
 class BasicReader{
     public static void main(String[] args){
-        while(true){
-            try{
-                BufferedReader filereader = new BufferedReader(new FileReader("file.txt"));
-                while(true){
-                    try{
-                        String line = filereader.readLine();
-                        if(line.equals("EOF")){
-                            break;
-                        }
-                        System.out.println(line);
-                    }catch(Exception e){
-                        System.out.println("Failed to read the line");
+        try{
+            BufferedReader filereader = new BufferedReader(new FileReader("file.txt")); //creating BufferedReader object to read the contents of the file
+            while(true){ //while loop to read all lines
+                try{
+                    String line = filereader.readLine(); //reading the next line
+                    if(line.equals("EOF")){
+                        break; //breaking the while loop if we have reached the end of the file
                     }
+                    System.out.println(line); //printing current line
+                }catch(Exception e){ //catching any errors that potentionaly might come up
+                    System.out.println("Failed to read the line");
+                    break; //breaking the while loop
                 }
-            }catch(FileNotFoundException e){
-                System.out.println("File not found");
             }
+            try{
+                filereader.close(); //closing the file
+            }catch(Exception e){ //catching any errors that potentionaly might come up
+                System.out.println("An error occured while closing file");
+            }
+        }catch(FileNotFoundException e){ //the file doesn't exists
+            System.out.println("File not found");
         }
     }
 }
