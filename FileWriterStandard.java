09a0c35ff4e24af7aa5765d994ae734d3dbb8b22
Marusel <62075300+MaruselPlay@users.noreply.github.com>
2024-01-19 13:15:02 +0000

    Update FileWriterStandard.java

--- FileWriterStandard.java
+++ FileWriterStandard.java
@@ -3,30 +3,36 @@ import java.io.FileWriter;
 
 class FileWriterStandard{
 
-  public String[] dataToWrite = {"Green", "Eggs", "and", "Ham", "EOF"};
+  public String[] dataToWrite = {"Green", "Eggs", "and", "Ham", "EOF"}; //declaring global array with pre-declared values
 
   public static void main(String[] args){
-    FileWriterStandard self = new FileWriterStandard();
-    self.start();
+    FileWriterStandard self = new FileWriterStandard(); //creating an instance of our class
+    self.start(); //calling method start inside the object
   }
 
   public void start(){
-    this.writeDataToFile(this.getFileNameFromUser());
+    this.writeDataToFile(this.getFileNameFromUser()); //getting file name from user and writing data to it
   }
 
+  /*
+  * The following function gets the file name from user and returns it as a String object
+  */
   public String getFileNameFromUser(){
     System.out.print("Enter the name of file: ");
     return new Scanner(System.in).nextLine();
   }
 
+  /*
+  * Writing data from global array to the given file
+  */
   public void writeDataToFile(String file){
-    try{
-      FileWriter fileWriter = new FileWriter(file);
+    try{//try and catch block to catch any potential errors
+      FileWriter fileWriter = new FileWriter(file); //initializing FileWriter instance
       for(int i = 0; i < dataToWrite.length; i++){
-        fileWriter.write(dataToWrite[i] + "\n");
+        fileWriter.write(dataToWrite[i] + "\n"); //writing all data from array on separates lines
       }
       fileWriter.close();
-    }catch(Exception e){
+    }catch(Exception e){//error handling 
       System.out.println("An error happend while writing to file: ");
       e.printStackTrace();
     }
