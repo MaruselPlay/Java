eb6b99c836480033ddec5c4670635366375df148
Marusel <62075300+MaruselPlay@users.noreply.github.com>
2024-01-26 14:03:13 +0000

    Update SecretCaeser.java

--- SecretCaeser.java
+++ SecretCaeser.java
@@ -12,25 +12,37 @@ class SecretCaeser{
     instance.runAll();
   }
 
+  /*
+  * This method is called at the start and runs the program
+  */
   public void runAll(){
     this.outPutASCII();
     this.encrypt();
     this.showEncrypted();
   }
 
+  /*
+  * This code converts global variable plaintext to acii and outputs it
+  */
   private void outPutASCII(){
     System.out.print("\"" + this.plaintext + "\" in ASCII is: ");
-    for(int i = 0; i < this.plaintext.length(); i++){
+    for(int i = 0; i < this.plaintext.length(); i++){ //a loop to convert each leytein the string
       System.out.print((int) this.plaintext.charAt(i));
     }
   }
 
+  /*
+  * Encrypt function, encrypts the variable plaintext and stores it in global variable
+  */
   private void encrypt(){
     for (int i = 0; i < this.plaintext.length(); i++) {
-      this.encrypted += (char) ((int) this.plaintext.charAt(i) + 10);
+      this.encrypted += (char) ((int) this.plaintext.charAt(i) + 10); //adding encrypted letter by letter
     }
   }
 
+  /*
+  * Outputs the contents of variable encrypted
+  */
   private void showEncrypted(){
     System.out.print("\nEncrypted: " + this.encrypted);
   }
