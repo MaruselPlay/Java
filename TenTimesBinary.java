70be6d018556b7b83363d4a9db8e4a5a6a27f6f8
Marusel <62075300+MaruselPlay@users.noreply.github.com>
2024-02-01 11:50:19 +0000

    update

---
+++ TenTimesBinary.java
@@ -0,0 +1,23 @@
+class TenTimesBinary{
+
+  private int[] intArray = new int[100];
+
+  public static void main(String[] args){
+
+  }
+
+  private void fillArray(){
+    for(int i = l i < this.intArray.length - 1; i++){
+      this.intArray[i] = i * 10;
+    }
+  }
+
+  private SearchResult binarySearch(int[] array, int searchValue){
+    
+  }
+}
+
+class SearchResult{
+  public int position = -1;
+  public boolean found = false;
+}
