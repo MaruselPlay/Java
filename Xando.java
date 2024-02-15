dfc154c54ade6348f09f52b327c77acf19d8ee08
MaruselPlay <maruselplay@gmail.com>
2024-02-15 22:29:57 +0000

    какие то изменения

--- Xando.java
+++ Xando.java
@@ -1,34 +1,41 @@
 class Xando{
 
   private String[][] board = new String[3][3];
+  private boolean running = true;
 
   public static void main(String[] args){
     Xando instance = new Xando();
-    instance.run();
+    instance.start();
   }
 
-  public void run(){
+  public void start(){
     this.clearBoard();
     this.printBoard();
+
+    while(this.running){
+      this.tick();
+    }
   }
 
-  private  
+  private void tick(){
+    
+  }
 
-  private boolean processTurn(int x, int y, Char entry){
+  private boolean processTurn(int x, int y, String entry){
     if(this.board[x][y] != "-"){
       return false;
     }
     this.board[x][y] = entry;
   }
 
-  private boolean celebrateWin(Char player){
+  private boolean celebrateWin(String player){
 
   }
 
   private boolean checkForWin(int x, int y){
-    Char searchFor = this.board[x][y];
+    String searchFor = this.board[x][y];
     int found = 0;
-    for(xx = x - 2; xx <= 2; xx++){
+    for(int xx = x - 2; xx <= 2; xx++){
       if(this.board[xx][y] == searchFor){
         found++;
         if(found == 3){
@@ -39,7 +46,7 @@ class Xando{
       }else{
         found = 0;
       }
-      for(yy = y - 2; yy <= 2; yy++){
+      for(int yy = y - 2; yy <= 2; yy++){
         if(xx < 0 || yy < 0){
           continue;
         }
