1089cd7ad700557786a5d71e1429b2c262c65f2e
MaruselPlay <maruselplay@gmail.com>
2024-02-29 23:32:34 +0000

    update

---
+++ MyModularLightbulb.java
@@ -0,0 +1,95 @@
+//package GuiLearningBlueJ;
+
+//importing swing library
+import javax.swing.*;
+
+//importing action listener
+import java.awt.*;
+import java.awt.event.*;
+
+public class MyModularLightbulb{
+  
+    private JFrame frame = new JFrame("Lightbulb switch"); //creating new instance of the JFrame object
+    private JPanel panel = new JPanel(null); //creating new instance of JPanel object
+    private JButton onButton = new JButton("On"); //creating new instance of JButton for on button
+    private JButton offButton = new JButton("Off"); //creating new instance of JButton for off button
+    private JLabel label = new JLabel(); //creating new JLabel instance for displaying the current state of the lightbulb
+    
+    public static void main(String[] args){
+        MyModularLightbulb instance = new MyModularLightbulb();
+        instance.start();
+    }
+    
+    /**
+     * First function that is called inside the class
+     */
+    public void start(){
+        this.init();
+        this.frame.setVisible(true);
+    }
+    
+    public void setEnabled(boolean value){
+        if(value){
+            this.label.setIcon(new ImageIcon("on.jpg"));
+        }else{
+            this.label.setIcon(new ImageIcon("off.jpg"));
+        }
+    }
+    
+    public void setLocationRelativeTo(Component componet, Dimension relativeTo){
+        Dimension componentSize = componet.getSize();
+        componet.setLocation((int) relativeTo.getWidth() / 2 - (int) componentSize.getWidth() / 2, (int) relativeTo.getHeight() / 2 - (int) componentSize.getHeight() / 2);
+    }
+    
+    public void setLocationRelativeTo(Component componet, Dimension relativeTo, int offsetX, int offsetY){
+        Dimension componentSize = componet.getSize();
+        componet.setLocation((int) relativeTo.getWidth() / 2 - (int) componentSize.getWidth() / 2 - offsetX, (int) relativeTo.getHeight() / 2 - (int) componentSize.getHeight() / 2 - offsetY);
+    }
+    
+    /**
+     * Initialise all the components of the GUI and event listeners
+     */
+    public void init(){
+        //initialises the frame
+        this.frame.setSize(600, 400);
+        this.setLocationRelativeTo(frame, Toolkit.getDefaultToolkit().getScreenSize()); //sets frame location relative to screen size (center location)
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setLayout(new GridLayout(1,1));
+        
+        //initialises the label and adds it to the panel
+        this.label.setIcon(new ImageIcon("off.jpg"));
+        this.label.setSize(100, 161);
+        this.setLocationRelativeTo(label, frame.getSize(), 0, 50); //sets label location to center of frame +50 pixels in y direction
+        this.panel.add(this.label);
+        
+        //initialises the on button and adds it to the panel
+        this.onButton.setSize(100, 50);
+        this.setLocationRelativeTo(onButton, frame.getSize(), -100, -50); //sets button location to center of frame -100 pixels in x direction and -20 pixels in y direction
+        this.onButton.addActionListener(AL->onSwitchOnClick());
+        this.panel.add(this.onButton);
+        
+        //initialises the off button and adds it to the panel
+        this.offButton.setSize(100, 50);
+        this.setLocationRelativeTo(offButton, frame.getSize(), 100, -50); //sets button location to center of frame +100 pixels in x direction and -20 pixels in y direction
+        this.offButton.addActionListener(AL->onSwitchOffClick());
+        this.panel.add(this.offButton);
+        
+        this.frame.add(this.panel); //adds the panel to the frame
+    }
+    
+    /**
+     * Called when off button is pressed
+     * Updates the text and the state of the lightbulb
+     */
+    public void onSwitchOffClick(){
+        this.setEnabled(false);
+    }
+    
+    /**
+     * Called when on button is pressed
+     * Updates the text and the state of the lightbulb
+     */
+    public void onSwitchOnClick(){
+        this.setEnabled(true);
+    }
+}
--- Safecracker.java
+++ Safecracker.java
@@ -3,29 +3,53 @@ import java.util.Random;
 
 class Safecracker{
 
-  private int[] code = new int[3];
-  private boolean running = true;
-  private Scanner scanner = new Scanner(System.in)
+  private int[] code = new int[3]; //global variable to hold code
+  private boolean running = true; //status of the program
+  private int maxAttempts = -1;
+  private boolean showHints = false;
+  private Scanner scanner = new Scanner(System.in);
 
+  /**
+   * Creating instance of this class and running it
+   */
   public static void main(String[] args){
     Safecracker instance = new Safecracker();
     instance.start();
   }
 
+  /**
+   * Starts the program and keeps it running
+   */
   public void start(){
     while(this.running){
       this.showOptionMenu();
+      System.out.print("Enter an option: ");
       this.handleOption(this.getOption());
     }
   }
 
+  /**
+   * Executes an action that should happen when specific option entered
+   * @param option
+   */
   private void handleOption(int option){
     switch(option){
       case 1:
-        this.trySaveCode(scanner.nextLine());
+        this.letUserEnterCode();
         break;
       case 2:
         this.generateCode();
+        break;
+      case 3:
+        this.letUserSetMaxAttempts();
+        break;
+      case 4:
+        if(this.showHints){
+          System.out.println("Hints were turned off!");
+        }else{
+          System.out.println("Hints were turned on!");
+        }
+        this.showHints = !this.showHints;
         break;
       case 5:
         this.letUserGuessCode();
@@ -35,77 +59,183 @@ class Safecracker{
     }
   }
 
-  private void letUserGuessCode(){
-    System.out.print("Try to guess the code: ");
-    boolean guessed = false;
-    String[] splitCode = new String[3];
-    String code;
-    while(!guessed){
-      code = scanner.nextLine();
-      if(code.length() != 3){
-        System.out.println("The code should be 3-digit");
+  /**
+   * Sets the maximum amount of attempts
+   * @param maxAttempts
+   */
+  public void setMaxAttempts(int maxAttempts){
+    this.maxAttempts = maxAttempts;
+  }
+
+  /**
+   * Returns the maximum amount of attempts
+   * @return int
+   */
+  public int getMaxAttempts(){
+    return this.maxAttempts;
+  }
+
+  /**
+   * Asks the user for the maximum number of attempts and makes sure valid value entered and stored
+   */
+  private void letUserSetMaxAttempts(){
+    System.out.print("Input max amount of attempts or enter 0 for unlimited: ");
+    boolean valid = false;
+    int attempts;
+    while(!valid){
+      try{
+        attempts = Integer.parseInt(this.scanner.nextLine());
+      }catch(Exception e){
+        System.out.println("Must be numeric, try again: ");
         continue;
       }
 
-      for(int i = 0; i < splitCode.length; i++){
-        splitCode[i] = code[i];
+      if(attempts < 0){
+        System.out.print("Can't be less than 0, try again: ");
+        continue;
       }
 
-      if(this.isCodeEqual(splitCode)){
-        System.out.print("Wrong code, try again: ");
+      if(attempts == 0){
+        this.maxAttempts = -1;
+        System.out.println("Unlimited number of attempts was set");
+      }else{
+        this.maxAttempts = attempts;
+        System.out.println("Maximum attempts of " + attempts + " was set");
       }
-      guessed = true;
+      valid = true;
     }
   }
 
+  /**
+   * Asks the user to guess the current code
+   */
+  private void letUserGuessCode(){
+    System.out.print("Try to guess the code: ");
+    boolean guessed = false;
+    String code;
+    int guesses = 0;
+    while(!guessed){
+      code = this.scanner.nextLine();
+      if(code.length() != 3){
+        System.out.print("The code should be 3-digit, try again: ");
+        continue;
+      }
+
+      if(!this.isCodeEqual(code.split(""))){
+        guesses++;
+        if(guesses == this.maxAttempts){
+          System.out.println("Wrong code, maximum amount of attempts exceeded");
+          return;
+        }else{
+          System.out.print("Wrong code, try again: ");
+        }
+        continue;
+      }
+      guessed = true;
+    }
+    System.out.println("You guessed it!");
+  }
+
+  /**
+   * Cheks if given code equal to the currently stored one
+   * @param code
+   * @return boolean
+   */
   private boolean isCodeEqual(String[] code){
+    boolean correct = true;
     try{
       for(int i = 0; i < code.length; i++){
         if(Integer.parseInt(code[i]) != this.code[i]){
-          return false;
+          correct = false;
+        }else{
+          if(this.showHints){
+            System.out.println("You gussed the right number at position " + (i + 1) + " it is number " + code[i]);
+          }
         }
       }
+    }catch(Exception e){
+      return false;
+    }
+    return correct;
+  }
+
+  /**
+   * Randomly generates 3-digit code
+   */
+  public void generateCode(){
+    Random random = new Random();
+    for(int i = 0; i < 3; i++){
+      this.code[i] = random.nextInt(9);
+    }
+    System.out.println("Random code was set");
+  }
+
+  /**
+   * Lets user enter the code to store. Ensures that data inputed is valid.
+   */
+  private void letUserEnterCode(){
+    System.out.print("Enter the code you want to set: ");
+    boolean valid = false;
+    String code;
+    String[] splitCode = new String[3];
+    loop:
+    while(!valid){
+      code = this.scanner.nextLine();
+      if(code.length() != 3){
+        System.out.print("The code should be 3-digit, try again: ");
+        continue;
+      }
+      
+      splitCode = code.split("");
+      for(int i = 0; i < splitCode.length; i++){
+        try{
+          this.code[i] = Integer.parseInt(splitCode[i]);
+        }catch(Exception e){
+          System.out.print("The code should be numeric, try again: ");
+          continue loop;
+        }
+      }
+      valid = true;
+    }
+  }
+
+  /**
+   * Displays option menu
+   */
+  public void showOptionMenu(){
+    System.out.println("1 - Manually set a new 3-digit code\n2 - Randomly generate a 3-digit code\n3 - Set max number of guesses\n4 - Turn hints on/off\n5 - Guess the code\n6 - Exit");
+  }
+
+  /**
+   * Sets given code as current code
+   * @param code
+   */
+  public void setCode(int[] code){
+    this.code = code;
+  }
+
+  /**
+   * Returns if the given string can be converted to integer
+   * @param String unparsedInt
+   * @return boolean
+   */
+  public boolean isInt(String unparsedInt){
+    try{
+      Integer.parseInt(unparsedInt);
     }catch(Exception e){
       return false;
     }
     return true;
   }
 
-  private void generateCode(){
-    Random random = new Random();
-    for(int i = 0; i < 3; i++){
-      this.code[i] = random.nextInt(9);
-    }
-  }
-
-  private void trySaveCode(String code){
-    if(code.length() != 3){
-      System.out.println("The code should be 3-digit");
-      return;
-    }
-
-    String[] splitCode = new String[3];
-    for(int i = 0; i < splitCode.length; i++){
-      try{
-        this.code[i] = Integer.parseInt(splitCode[i]);
-      }catch(Exception e){
-        System.out.println("The code should be numeric");
-        return;
-      }
-    }
-  }
-
-  private void showOptionMenu(){
-    System.out.println("1 - Manually set a new 3-digit code\n2 - Randomly generate a 3-digit code\n3 - Set max number of guesses\n4 - Turn hints on/off\n5 - Guess the code\n6 - Exit");
-  }
-
-  private void setCode(int[] code){
-    this.code = code;
-  }
-
+  /**
+   * Reads input from the user
+   * @return int
+   */
   private int getOption(){
     String option = this.scanner.nextLine();
     int numericOption = 0;
+    
     try{
       numericOption = Integer.parseInt(option);
     }catch(Exception e){
